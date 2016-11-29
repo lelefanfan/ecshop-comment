945de9ba91248715c564366350c8125560d9cc26
lelefanfan5 <lelefanfan520@qq.com>
2016-11-29 21:54:49 +0800

    '初始化第一次提交'

---
+++ data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ec3";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "root";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','e9f4853eb1aa8cdf8a65ef6e676209fb');
+
+?>
